7238a4efb91159359043c5b2d5d175d026e2b3cf
Melanie <makes.melly@gmail.com>
2014-10-11 22:20:31 +0200

    Added empty foodguide

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,4 +6,7 @@ class PagesController < ApplicationController
   def nitrous
   end
 
+  def foodguide
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
 
-  get "railspetguide" => "pages#guide",   :as => "railspetguide"
-  get "nitrous"       => "pages#nitrous", :as => "nitrous"
+  get "railspetguide" => "pages#guide",     :as => "railspetguide"
+  get "nitrous"       => "pages#nitrous",   :as => "nitrous"
+  get "foodguide"     => "pages#foodguide", :as => "foodguide"
 
 
 end
